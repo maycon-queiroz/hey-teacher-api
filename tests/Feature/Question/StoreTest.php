45a2c4ed56ddbb79981c722e4cf2840971dd8800
Maycon Queiroz <fmaycon@gmail.com>
2024-11-03 19:42:54 -0300

    API-2: Add validation test for required question field
    Refactored an existing test to use Pest's `test` function for consistency. Introduced a new test to ensure the `question` field is required during question creation.
    
    Signed-off-by: Maycon Queiroz <fmaycon@gmail.com>

--- tests/Feature/Question/StoreTest.php
+++ tests/Feature/Question/StoreTest.php
@@ -3,6 +3,7 @@
 namespace Tests\Feature\Question;
 
 use App\Models\User;
+
 use Laravel\Sanctum\Sanctum;
 
 use function Pest\Laravel\{assertDatabaseHas, postJson};
@@ -22,7 +23,7 @@ it('should be a able to store a question', function () {
     ]);
 });
 
-it('after creating a new question, I need to make sure that it creates on _draft_ status', function () {
+test('after creating a new question, I need to make sure that it creates on _draft_ status', function () {
     $user = User::factory()->create();
 
     Sanctum::actingAs($user);
@@ -32,8 +33,21 @@ it('after creating a new question, I need to make sure that it creates on _draft
     ])->assertSuccessful();
 
     assertDatabaseHas('questions', [
-        'user_id' => $user->id,
+        'user_id'  => $user->id,
         'question' => 'How to create a question?',
-        'status' => 0,
+        'status'   => 0,
     ]);
+});
+
+describe('validation rules', function () {
+    test('question::required', function () {
+        $user = User::factory()->create();
+
+        Sanctum::actingAs($user);
+
+        postJson(route('questions.store'), [])
+            ->assertJsonValidationErrors([
+                'question' => 'The question field is required.',
+            ]);
+    });
 });
