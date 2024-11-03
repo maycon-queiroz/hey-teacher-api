e930be6ef2cbf3dcfa5ca08c972af652f52e9a6a
Maycon Queiroz <fmaycon@gmail.com>
2024-11-03 19:26:04 -0300

    API-2: Add test to ensure new questions default to draft status
    This commit adds a test to verify that newly created questions are set to draft status by default. It checks that the status field is set to 0 in the database for new questions created by users.
    
    Signed-off-by: Maycon Queiroz <fmaycon@gmail.com>

--- tests/Feature/Question/StoreTest.php
+++ tests/Feature/Question/StoreTest.php
@@ -21,3 +21,19 @@ it('should be a able to store a question', function () {
         'question' => 'How to create a question?',
     ]);
 });
+
+it('after creating a new question, I need to make sure that it creates on _draft_ status', function () {
+    $user = User::factory()->create();
+
+    Sanctum::actingAs($user);
+
+    postJson(route('questions.store'), [
+        'question' => 'How to create a question?',
+    ])->assertSuccessful();
+
+    assertDatabaseHas('questions', [
+        'user_id' => $user->id,
+        'question' => 'How to create a question?',
+        'status' => 0,
+    ]);
+});
