76d9dac88ed880846d6e87d89e1c994d60a23239
Maycon Queiroz <fmaycon@gmail.com>
2024-11-24 16:53:18 -0300

    API-22: feat: should be able restore  a question

---
+++ tests/Feature/Question/RestoreTest.php
@@ -0,0 +1,40 @@
+<?php
+
+use App\Models\{Question, User};
+use Laravel\Sanctum\Sanctum;
+
+use function Pest\Laravel\{assertNotSoftDeleted, assertSoftDeleted, putJson};
+
+test('should be able to restore a question', function () {
+    $user     = User::factory()->create();
+    $question = Question::Factory()->for($user, 'user')->create();
+
+    Sanctum::actingAs($user);
+
+    $question->delete();
+
+    assertSoftDeleted('questions', ['id' => $question->id]);
+
+    $response = putJson(route('questions.restore', $question));
+    $response->assertOk();
+
+    assertNotSoftDeleted('questions', ['id' => $question->id]);
+    $response->assertJsonFragment(['message' => 'Question restored successfully']);
+
+});
+
+test('should be able to restore if user was created', function () {
+    $user      = User::factory()->create();
+    $userWrong = User::factory()->create();
+    $question  = Question::Factory()->for($user, 'user')->create();
+
+    $question->delete();
+    assertSoftDeleted('questions', ['id' => $question->id]);
+
+    Sanctum::actingAs($userWrong);
+
+    putJson(route('questions.restore', $question))
+        ->assertForbidden();
+
+    assertSoftDeleted('questions', ['id' => $question->id]);
+});
