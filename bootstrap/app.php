4561e8305e30f057d27265a5f0e971c6f9dfc3fc
Maycon Queiroz <fmaycon@gmail.com>
2024-12-15 22:26:27 -0300

    API-18: reafactor config sanctum

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,7 +12,7 @@ return Application::configure(basePath: dirname(__DIR__))
         apiPrefix: '/',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        $middleware->statefulApi();
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
